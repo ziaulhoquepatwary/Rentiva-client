72ef23d2b6957ac8c10d82a98ffc74b69b7f2d42
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-06-18 14:08:45 +0600

    Create ThemeToggle Controller and use auth section

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import ThemeToggle from "@/components/ThemeToggle";
+
+
+function AuthLayout({ children }) {
+    return (
+        <div className="auth-container min-h-screen bg-white dark:bg-[#0f291e] transition-colors duration-300">
+            {/* Theme Toggle - Top Right */}
+            <div className="fixed top-6 right-6 z-50">
+                <ThemeToggle />
+            </div>
+            {children}
+        </div>
+    )
+}
+
+export default AuthLayout
